a818f48246071c6314d867414766912a6cc8e815
Oyebanji Oluwadamilare <ocoyebanji@irissmart.com>
2020-11-14 13:16:36 +0100

    Initial commit

---
+++ ReportLogService/ReportLogAPI/Extentions/ServiceExtention.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ReportLogEntityFrameworkCore.AppContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReportLogAPI.Extentions
+{
+	//Service Extention to be registerd in  Startup Class 
+	//to addservices to the container.
+	public static class ServiceExtention
+	{
+		public static IServiceCollection ConfigureCors(this IServiceCollection services)
+		{
+			services.AddCors(options =>
+			{
+				options.AddPolicy("CorsPolicy",
+					builder => builder.AllowAnyOrigin()
+					.AllowAnyMethod()
+					.AllowAnyHeader());
+			});
+
+			return services;
+		}
+
+		//Wire up SQL Connection String
+		public static IServiceCollection ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddDbContext<ReportLogDbContext>(
+				options => options.UseSqlServer(configuration.GetConnectionString("Localsource"))
+				);
+			return services;
+		}
+	}
+}
---
+++ ReportLogService/ReportLogEntityFrameworkCore/AppContext/ReportLogDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReportLogEntityFrameworkCore.AppContext
+{
+	public class ReportLogDbContext : DbContext
+	{
+		public ReportLogDbContext(DbContextOptions options) : base(options)
+		{
+
+		}
+
+		protected override	void OnModelCreating(ModelBuilder builder)
+		{
+
+		}
+	}
+}
